0bfc1efd40c0ee3d7ecbf60036cf286297abe956
randomguy <>
2021-08-17 14:32:22 +0200

    small update

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name = "fundamentals vs market price",
-    version = "1.0",
+    version = "1.2",
     description = "work in progress",
     packages = find_packages(),
-    install_requires = ['pandas', 'openpyxl']
+    install_requires = ['pandas', 'openpyxl', 'yahoo_fin']
 )
