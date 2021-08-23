c82aa5e0d4c652c8422880c4eff5202ab1bde9ef
randomguy <>
2021-08-23 19:38:20 +0200

    small update

--- main.py
+++ main.py
@@ -3,15 +3,19 @@
 import checks
 print('0 checks - done')
 
+print('1 prices_updated - initiating')
 import prices_updated
 print('1 prices_updated - done')
 
+print('2 prices_additional_calc - initiating')
 import prices_additional_calc
 print('2 prices_additional_calc - done')
 
+print('3 narrowed_filter - initiating')
 import narrowed_filter
 print('3 narrowed_filter - done')
 
+print('4 prepare_financials - initiating')
 import prepare_financials
 print('4 prepare_financials - done')
 
