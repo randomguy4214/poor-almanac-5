361e08596d4e0b5182361d91ed4edce7aa028270
randomguy <randomguy@randomguy.com>
2021-09-04 09:08:14 +0200

    fixed TTM

--- datasets_merge.py
+++ datasets_merge.py
@@ -11,18 +11,41 @@ prices_folder = "data"
 output_folder = "0_output"
 temp_folder = "temp"
 
+print('starting merging the datasets')
 # import
 fundamentals_table = pd.read_csv(os.path.join(cwd,input_folder,"3_fundamentals_processed.csv"), low_memory=False)
 prices_table = pd.read_csv(os.path.join(cwd,input_folder,"2_prices_updated.csv"), low_memory=False)
-#fundamentals_table = fundamentals_table.head()
+#fundamentals_table = fundamentals_table.head(400)
 #prices_table = prices_table.head()
+print("importing fundamentals and prices is done")
 
-# merge
+# adding TTM from t0,t1,t2,t3 before selecting t0 only for income and cash flows
+df_ttm = fundamentals_table.groupby(['symbol'])[['totalRevenue', 'costOfRevenue'
+                                        ,'totalCashFromOperatingActivities', 'capitalExpenditures'
+                                        , 'totalOperatingExpenses', 'totalCashflowsFromInvestingActivities']].sum()
+df_ttm = df_ttm.reset_index(drop=False)
+df_ttm['capitalExpenditures'].fillna(df_ttm['totalCashflowsFromInvestingActivities'], inplace=True)
+df_ttm.rename(columns={'totalRevenue': 'totalRevenueTTM', 'costOfRevenue': 'costOfRevenueTTM'
+    , 'totalCashFromOperatingActivities': 'totalCashFromOperatingActivitiesTTM'
+    , 'totalOperatingExpenses': 'totalOperatingExpensesTTM'
+    , 'capitalExpenditures': 'capitalExpendituresTTM' }, inplace=True)
+print("ttm precalculated")
+
+# select only latest data to filter out balance sheet
+fundamentals_table = fundamentals_table[fundamentals_table['Period'] == "t0"]
+print("fundamentals_table period = t0")
+
+# merge fundamentals and prices
 df_merged = pd.merge(fundamentals_table, prices_table, how='inner', left_on=['symbol'], right_on=['symbol'], suffixes=('', '_drop'))
 df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
 df_merged.rename(columns={'52 Week High 3': '52h', '52 Week Low 3': '52l', 'Quote Price': 'price'}, inplace=True)
-df_merged['price'].fillna(df_merged['Previous Close'], inplace=True)
-df_merged['NAV'].fillna(df_merged['totalStockholderEquity'], inplace=True)
+print("fundamentals and prices merged")
+
+# merge TTM
+df_to_merge = df_merged
+df_merged = pd.merge(df_to_merge, df_ttm, how='inner', left_on=['symbol'], right_on=['symbol'], suffixes=('', '_drop'))
+df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
+print("ttm merged")
 
 # fillna
 cols_to_format = [i for i in df_merged.columns]
@@ -34,25 +57,92 @@ for col in cols_to_format:
             pass
     except:
         pass
-
 # adding from low/high
+df_merged['price'].fillna(df_merged['Previous Close'], inplace=True)
 df_merged['from_low'] = (df_merged['price'] - df_merged['52l'])/df_merged['52l'] * 100
 df_merged['from_high'] = (df_merged['price'] - df_merged['52h'])/df_merged['52h'] * 100
+print('added low/high')
 
-# adding TTM
-df_ttm = df_merged.groupby(['symbol'])[['totalRevenue', 'costOfRevenue'
-                                        ,'totalCashFromOperatingActivities', 'capitalExpenditures'
-                                        , 'totalOperatingExpenses']].sum()
-df_ttm = df_ttm.reset_index(drop=False)
-df_ttm.rename(columns={'totalRevenue': 'totalRevenueTTM', 'costOfRevenue': 'costOfRevenueTTM'
-    , 'totalCashFromOperatingActivities': 'totalCashFromOperatingActivitiesTTM'
-    , 'totalOperatingExpenses': 'totalOperatingExpensesTTM'
-    , 'capitalExpenditures': 'capitalExpendituresTTM' }, inplace=True)
+# find latest shorts value
+df_shorts = pd.DataFrame(df_merged.filter(regex='Short % of Float|symbol')).iloc[:,:]
+df_shorts.dropna(how='all', axis=1, inplace=True)
+#df_shorts_names = df_shorts.columns.str.strip('Short % of Float')
+df_shorts_names = df_shorts.columns.str.extract('.*\((.*)\).*')
+df_shorts_names.rename(columns={ df_shorts_names.columns[0]: "date" }, inplace = True)
+df_shorts_names_dates = pd.to_datetime(df_shorts_names['date'])#, errors='coerce')
+df_shorts.columns = df_shorts_names_dates
+df_shorts_names_dates = df_shorts_names_dates.sort_values(ascending=False)
+df_shorts = df_shorts[df_shorts_names_dates]
+df_shorts.columns = [*df_shorts.columns[:-1], 'symbol']
+df_shorts = df_shorts.melt(id_vars=["symbol"], var_name="Date")
+df_shorts = df_shorts.dropna(axis = 0)
+df_shorts.columns = [*df_shorts.columns[:-1], 'Short%']
+#df_shorts.to_excel(os.path.join(cwd,input_folder,'4_df_shorts.xlsx'), index=False)
+print("shorts calculated")
 
-# merging TTM
+# merge shorts
 df_to_merge = df_merged
-df_merged = pd.merge(df_to_merge, df_ttm, how='inner', left_on=['symbol'], right_on=['symbol'], suffixes=('', '_drop'))
+df_merged = pd.merge(df_to_merge, df_shorts, how='left', left_on=['symbol'], right_on=['symbol'], suffixes=('', '_drop'))
 df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
+df_merged.drop_duplicates()
+df_merged.reset_index(drop=True, inplace=True)
+#df_merged.to_excel(os.path.join(cwd,input_folder,'5_df_shorts.xlsx'), index=False)
+print("shorts merged")
+
+# start creating new variables
+df = df_merged
+# process
+df['NAV'].fillna(df['totalStockholderEquity'], inplace=True)
+df['Short%'] = df['Short%'].str.rstrip('%').str.replace(',','').astype('float')
+df['OpMarg'] = ((df['totalRevenueTTM'] - df['costOfRevenueTTM']) / df['totalRevenueTTM'] * 100).astype('float')
+df['%Ins'] = df['% Held by Insiders 1'].str.rstrip('%').str.replace(',','').astype('float')
+df['BVPS'] = df['Book Value Per Share (mrq)']
+
+# fix from https://stackoverflow.com/questions/39684548/convert-the-string-2-90k-to-2900-or-5-2m-to-5200000-in-pandas-dataframe
+df['Debt'] = (df['Total Debt (mrq)'].replace(r'[ktmbKTMB]+$', '', regex=True).astype(float) *
+            df['Total Debt (mrq)'].str.extract(r'[\d\.]+([ktmbKTMB]+)', expand=False).fillna(1).replace(
+            ['k', 't', 'm', 'b', 'K', 'T', 'M', 'B']
+            , [10**3, 10**3, 10**6, 10**9, 10**3, 10**3, 10**6, 10**9]).astype(int))
+
+df['SO'] = (df['Shares Outstanding 5'].replace(r'[ktmbKTMB]+$', '', regex=True).astype(float) *
+            df['Shares Outstanding 5'].str.extract(r'[\d\.]+([ktmbKTMB]+)', expand=False).fillna(1).replace(
+            ['k', 't', 'm', 'b', 'K', 'T', 'M', 'B']
+            , [10**3, 10**3, 10**6, 10**9, 10**3, 10**3, 10**6, 10**9]).astype(int))
+
+# calculate additional variables
+df['NAV_per_share'] = df['NAV'] / df['sharesOutstanding']
+df['B/S/P'] = df['NAV_per_share'] / df['price']
+df['OwnEa/S'] = (df['totalCashFromOperatingActivitiesTTM'] - df['capitalExpendituresTTM']) / df['sharesOutstanding']
+df['OwnEa/S/P'] = df['OwnEa/S'] / df['price']
+df['marg'] = (df['totalRevenueTTM'] - df['totalOperatingExpensesTTM']) / df['totalRevenueTTM'] * 100
+df['WC/S'] = df['WC'] / df['sharesOutstanding']
+df['WC/S/P'] = df['WC/S'] / df['price']
+df['WC/Debt'] = df['WC'] / df['Debt']
+df['Rev/S/P'] = df['Revenue Per Share (ttm)'] / df['price']
+print('additional variables calculated')
+
+# fillna again
+cols_to_format = [i for i in df.columns]
+for col in cols_to_format:
+    try:
+        if col in ['price', 'from_low', 'from_high', 'OpMarg', 'B/S/P', 'marg']:
+            df[col]=df[col].fillna(0)
+        else:
+            pass
+    except:
+        pass
+
+# format
+cols_to_format = [i for i in df.columns]
+for col in cols_to_format:
+    try:
+        if col in ['price', 'B/S/P']:
+            df[col]=df[col].round(2)
+        else:
+            df[col] = df[col].round(0)
+    except:
+        pass
+print('formatting is done')
 
 # reorder and export
 cols_to_order = ['symbol', 'price', '52l', '52h', 'from_low', 'from_high']
@@ -64,3 +154,4 @@ df_merged.to_excel(os.path.join(cwd,input_folder,"4_merged.xlsx"))
 # export tickers again. just to have more narrowed result
 stocks = df_merged[['symbol']].sort_values(by=['symbol'], ascending= True).drop_duplicates()
 stocks.to_csv(os.path.join(cwd,input_folder,"4_tickers_filtered.csv"), index = False)
+print("datasets are merged")
--- output.py
+++ output.py
@@ -17,63 +17,6 @@ input_folder = "0_input"
 drop_list = pd.read_excel(os.path.join(cwd,input_folder,"0_drop_list.xlsx"))
 df = pd.read_csv(os.path.join(cwd,input_folder,"4_merged.csv"), low_memory=False)
 
-# select only latest data
-df = df[df['Period'] == "t0"]
-
-# find latest shorts value
-df_shorts = pd.DataFrame(df.filter(regex='Short % of Float|symbol')).iloc[:,:]
-df_shorts.dropna(how='all', axis=1, inplace=True)
-#df_shorts_names = df_shorts.columns.str.strip('Short % of Float')
-df_shorts_names = df_shorts.columns.str.extract('.*\((.*)\).*')
-df_shorts_names.rename(columns={ df_shorts_names.columns[0]: "date" }, inplace = True)
-df_shorts_names_dates = pd.to_datetime(df_shorts_names['date'])#, errors='coerce')
-df_shorts.columns = df_shorts_names_dates
-df_shorts_names_dates = df_shorts_names_dates.sort_values(ascending=False)
-df_shorts = df_shorts[df_shorts_names_dates]
-df_shorts.columns = [*df_shorts.columns[:-1], 'symbol']
-df_shorts = df_shorts.melt(id_vars=["symbol"], var_name="Date")
-df_shorts = df_shorts.dropna(axis = 0)
-df_shorts.columns = [*df_shorts.columns[:-1], 'Short%']
-#df_shorts.to_excel(os.path.join(cwd,input_folder,'5_df_shorts.xlsx'), index=False)
-
-# merge this shit
-df_merged = pd.merge(df, df_shorts, how='left', left_on=['symbol'], right_on=['symbol'], suffixes=('', '_drop'))
-df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
-df_merged.drop_duplicates()
-df_merged.reset_index(drop=True, inplace=True)
-#df_merged.to_excel(os.path.join(cwd,input_folder,'5_df_shorts.xlsx'), index=False)
-
-# finally some action
-df = df_merged
-
-# process
-df['Short%'] = df['Short%'].str.rstrip('%').str.replace(',','').astype('float')
-df['OpMarg'] = ((df['totalRevenueTTM'] - df['costOfRevenueTTM']) / df['totalRevenueTTM'] * 100).astype('float')
-df['%Ins'] = df['% Held by Insiders 1'].str.rstrip('%').str.replace(',','').astype('float')
-df['BVPS'] = df['Book Value Per Share (mrq)']
-
-# fix from https://stackoverflow.com/questions/39684548/convert-the-string-2-90k-to-2900-or-5-2m-to-5200000-in-pandas-dataframe
-df['Debt'] = (df['Total Debt (mrq)'].replace(r'[ktmbKTMB]+$', '', regex=True).astype(float) *
-            df['Total Debt (mrq)'].str.extract(r'[\d\.]+([ktmbKTMB]+)', expand=False).fillna(1).replace(
-            ['k', 't', 'm', 'b', 'K', 'T', 'M', 'B']
-            , [10**3, 10**3, 10**6, 10**9, 10**3, 10**3, 10**6, 10**9]).astype(int))
-
-df['SO'] = (df['Shares Outstanding 5'].replace(r'[ktmbKTMB]+$', '', regex=True).astype(float) *
-            df['Shares Outstanding 5'].str.extract(r'[\d\.]+([ktmbKTMB]+)', expand=False).fillna(1).replace(
-            ['k', 't', 'm', 'b', 'K', 'T', 'M', 'B']
-            , [10**3, 10**3, 10**6, 10**9, 10**3, 10**3, 10**6, 10**9]).astype(int))
-
-# calculate additional variables
-df['NAV_per_share'] = df['NAV'] / df['sharesOutstanding']
-df['B/S/P'] = df['NAV_per_share'] / df['price']
-df['FCF/S'] = (df['totalCashFromOperatingActivitiesTTM'] - df['capitalExpendituresTTM']) / df['sharesOutstanding']
-df['FCF/S/P'] = df['FCF/S'] / df['price']
-df['marg'] = (df['totalRevenueTTM'] - df['totalOperatingExpensesTTM']) / df['totalRevenueTTM'] * 100
-df['WC/S'] = df['WC'] / df['sharesOutstanding']
-df['WC/S/P'] = df['WC/S'] / df['price']
-df['WC/Debt'] = df['WC'] / df['Debt']
-df['Rev/S/P'] = df['Revenue Per Share (ttm)'] / df['price']
-
 # reorder and select relevant columns
 cols_to_order = ['symbol', 'price'
     , 'from_low', 'from_high'
@@ -82,38 +25,16 @@ cols_to_order = ['symbol', 'price'
     , 'Short%', '%Ins'
     , 'B/S/P', 'BVPS', 'Rev/S/P'
     , 'WC/S/P'
-    , 'WC/Debt', 'Total Debt (mrq)', 'FCF/S/P'
+    , 'WC/Debt', 'Total Debt (mrq)', 'OwnEa/S/P'
     ]
 
 # reorder
 new_columns = cols_to_order + (df.columns.drop(cols_to_order).tolist())
 df_export = df[cols_to_order]
 
-# fillna
-cols_to_format = [i for i in df_export.columns]
-for col in cols_to_format:
-    try:
-        if col in ['price', 'from_low', 'from_high', 'OpMarg', 'B/S/P']:
-            df_export[col]=df_export[col].fillna(0)
-        else:
-            pass
-    except:
-        pass
-
-# format
-cols_to_format = [i for i in df_export.columns]
-for col in cols_to_format:
-    try:
-        if col in ['price', 'B/S/P']:
-            df_export[col]=df_export[col].round(2)
-        else:
-            df_export[col] = df_export[col].round(0)
-    except:
-        pass
-
 # sort and export unfiltered
 df_export.sort_values(by=['from_low'], ascending=[True], inplace=True, na_position ='last')
-df_export.to_excel(os.path.join(cwd,input_folder,'5_df_output_unflitered.xlsx'), index=False)
+df_export.to_excel(os.path.join(cwd,'5_df_output_unflitered.xlsx'), index=False)
 
 # filter by drop list
 drop_list_ticker = drop_list['symbol'].tolist()
@@ -130,4 +51,4 @@ df_export = df_export.loc[(df_export['price'] < 5)] # less than 5 bucks
 df_export = df_export.loc[df_export['B/S/P'] > 0.6] # Book to market
 
 # export
-df_export.to_excel(os.path.join(cwd,input_folder,'5_df_output.xlsx'), index=False)
+df_export.to_excel(os.path.join(cwd,'5_df_output.xlsx'), index=False)
