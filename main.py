35bf5aa29413eadf33124f620f4b73b09dd6ef28
randomguy <randomguy@randomguy.com>
2021-09-07 16:59:26 +0200

    -

--- main.py
+++ main.py
@@ -3,21 +3,32 @@
 import checks
 print('0 checks - done')
 
+
 print('prices_updated - initiating. Printing Stock and % Progress.')
 import prices_update
 print('prices_updated - done')
-
-print('prices_combine - initiating.')
-import prices_process
-print('prices_combine - done')
 
 print('financials_update - initiating. Printing Stock and % Progress.')
 import financials_update
 print('financials_update - done')
 
+print('financials_update - initiating. Printing Stock and % Progress.')
+import financials_update_annually
+print('financials_update - done')
+
+
+print('prices_combine - initiating.')
+import prices_process
+print('prices_combine - done')
+
 print('financials_combine - initiating.')
 import financials_process
 print('financials_combine - done')
+
+print('financials_combine - initiating.')
+import financials_process_annually
+print('financials_combine - done')
+
 
 print('datasets_merge - initiating.')
 import datasets_merge
