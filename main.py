c5f2705c62281ea1afe4eba082ba16880011d5f3
randomguy4214 <87262675+randomguy4214@users.noreply.github.com>
2021-12-28 14:19:50 +0100

    Update main.py
    LARGE UPDATE
    multithreading for fundamentals import
    remembering last ticker imported

--- main.py
+++ main.py
@@ -13,19 +13,25 @@ print('prices_process - initiating.')
 from bundle_main import prices_process
 print('prices_process - done')
 
+# multithreaded import of fundamentals quarterly and annually.
+from threading import Thread
+def a():
+    print('financials_process_quarterly - initiating.')
+    from bundle_main import financials_process_quarterly
+    print('financials_process_quarterly - done')
+def b():
+    print('financials_update_quarterly - initiating. Printing Stock and % Progress.')
+    from bundle_main import financials_update_quarterly
+    print('financials_update_quarterly - done')
 
-print('financials_process_quarterly - initiating.')
-from bundle_main import financials_process_quarterly
-print('financials_process_quarterly - done')
+Thread(target=a).start()
+Thread(target=b).start()
+
+
 
 print('financials_process_annually - initiating.')
 from bundle_main import financials_process_annually
 print('financials_process_annually - done')
-
-
-print('financials_update_quarterly - initiating. Printing Stock and % Progress.')
-from bundle_main import financials_update_quarterly
-print('financials_update_quarterly - done')
 
 print('financials_update_annually - initiating. Printing Stock and % Progress.')
 from bundle_main import financials_update_annually
