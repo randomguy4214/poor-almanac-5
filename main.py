871d1184c31332ce65b85912592ac8f7c0f9b72f
randomguy <random@guy>
2021-12-30 17:32:56 +0100

    -

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def a():
     from bundle_main import prices_process
 def b():
     from bundle_main import financials_update_quarterly
-    from bundle_main import financials_process_quarterly
 def c():
     from bundle_main import financials_update_annually
     from bundle_main import financials_process_annually
@@ -26,6 +25,7 @@ Thread(target=a).join()
 Thread(target=b).join()
 Thread(target=c).join()
 
+from bundle_main import financials_process_quarterly
 from bundle_main import datasets_merge
 from bundle_main import output
 
